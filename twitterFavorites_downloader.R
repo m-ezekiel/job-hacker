7a6f91935054176e5e2028793f0470c3c133fd38
m-ezekiel <git@m-ezekiel.com>
2017-08-24 11:40:06 -0500

    Add friends and favorites downloaders.

---
+++ twitterFavorites_downloader.R
@@ -0,0 +1,29 @@
+# Twitter Favorites Downloader
+# c. 2017-08-24
+
+library(dplyr)
+source("twitterConfig.R")
+rm(list = ls())
+
+# Define User
+userName <- "m_ezkiel"
+
+# Download Data (rate limited to 200)
+user <- getUser(userName)
+count <- user$favoritesCount; count
+user$getFavorites(n=count) -> userFavorites
+twListToDF(userFavorites) -> favs
+
+# Reshape Data
+names(favs)
+attach(favs)
+favoritesList <- data.frame(text=text,
+                            favCount=favoriteCount,
+                            rtCount=retweetCount,
+                            author=screenName,
+                            created=created)
+detach(favs)
+
+# Write Data
+filename <- paste0("favsList_data/", userName, "_favoritesList.csv")
+write.csv(favoritesList, file = filename)
---
+++ twitterFriends_downloader.R
@@ -0,0 +1,42 @@
+# Twitter Friends Downloader
+# c. 2017-08-24
+
+library(dplyr)
+source("twitterConfig.R")
+rm(list = ls())
+
+# Define User
+userName <- "m_ezkiel"
+
+# Download Data
+user <- getUser(userName)
+count <- user$friendsCount; count
+user$getFriends(n=count) -> userFriends
+twListToDF(userFriends) -> ufl
+
+# Reshape Data
+attach(ufl)
+friendList <- data.frame(name=name, 
+                         screenName=screenName,
+                         description=description,
+                         statuses=statusesCount,
+                         favorites=favoritesCount,
+                         friends=friendsCount,
+                         followers=followersCount,
+                         listed=listedCount,
+                         created=created,
+                         protected=protected,
+                         verified=verified,
+                         language=lang,
+                         location=location,
+                         profileImage=profileImageUrl,
+                         url=url)
+detach(ufl)
+
+
+# Write Data
+filename <- paste0("friendList_data/", userName, "_friendList.csv")
+write.csv(friendList, file = filename)
+
+favs <- user$getFavorites(n = 756)
+twListToDF(favs) -> favs_df
